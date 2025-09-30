eddd0af9020a6911da70da15b6b5d3852c4be05c
Imammahmuda1804 <imammahmuda11@gmail.com>
2025-09-30 09:53:53 +0700

    perubahan migration courses

--- database/migrations/2025_09_23_073959_create_courses_table.php
+++ database/migrations/2025_09_23_073959_create_courses_table.php
@@ -1,64 +1,35 @@
 <?php
 
-namespace App\Models;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
-use Illuminate\Database\Eloquent\Relations\HasMany;
-use Illuminate\Database\Eloquent\Relations\BelongsToMany;
-
-class Course extends Model
+return new class extends Migration
 {
-    use HasFactory;
-
-    protected $fillable = [
-        'name',
-        'slug',
-        'thumbnail',
-        'about',
-        'category_id',
-        'is_popular',
-    ];
-
-    public function category(): BelongsTo
+    /**
+     * Run the migrations.
+     *
+     */
+    public function up()
     {
-        return $this->belongsTo(Category::class);
+        Schema::create('courses', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('slug')->unique();
+            $table->string('thumbnail');
+            $table->text('about');
+            $table->boolean('is_popular');
+            $table->foreignId('category_id')->constrained('categories')->onDelete('cascade');
+            $table->timestamps();
+        });
     }
 
-    // Menggunakan relasi BelongsToMany untuk mentor
-    public function mentors(): BelongsToMany
+    /**
+     * Reverse the migrations.
+     *
+     */
+    public function down()
     {
-        return $this->belongsToMany(User::class, 'course_mentors', 'course_id', 'user_id');
+        Schema::dropIfExists('courses');
     }
-
-    public function course_benefits(): HasMany
-    {
-        return $this->hasMany(CourseBenefit::class);
-    }
-
-    public function sections(): HasMany
-    {
-        return $this->hasMany(CourseSection::class);
-    }
-
-    public function course_batches(): HasMany
-    {
-        return $this->hasMany(CourseBatch::class);
-    }
-
-    public function course_students(): HasMany
-    {
-        return $this->hasMany(CourseStudent::class);
-    }
-
-    public function course_pricings(): HasMany
-    {
-        return $this->hasMany(CoursePricing::class);
-    }
-
-    public function transactions(): HasMany
-    {
-        return $this->hasMany(Transaction::class);
-    }
-}
+};
